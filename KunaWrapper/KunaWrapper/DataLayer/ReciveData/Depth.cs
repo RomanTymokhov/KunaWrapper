08195df1404e3c3570573f4876888ccb7a13bb88
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-04 16:13:51 +0300

    Объект стакана

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/Depth.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper.DataLayer.ReciveData
+{
+    public class Depth
+    {
+        [JsonProperty(PropertyName = "timestamp")]
+        public int timestamp;
+
+        [JsonProperty(PropertyName = "asks")]
+        public List<KeyValuePair<string, string>> asks;
+
+        [JsonProperty(PropertyName = "bids")]
+        public List<KeyValuePair<string, string>> bids;
+    }
+}
