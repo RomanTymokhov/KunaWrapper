d82fa0740c9f6ac5bd16acceae37486d9329829b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-04 15:13:32 +0300

    Объект совершенного торга в стакане

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/Trade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+using KunaWrapper.DataLayer.Enums;
+
+namespace CunaWrapper.DataLevel.RciveData
+{
+    public class Trade
+    {
+        [JsonProperty(PropertyName = "id")]
+        public uint tradeId;
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal price;
+
+        [JsonProperty(PropertyName = "volume")]
+        public decimal volume;
+
+        [JsonProperty(PropertyName = "funds")]
+        public decimal funds;
+
+        [JsonProperty(PropertyName = "market")]
+        public MarketPair marketPair;
+
+        [JsonProperty(PropertyName = "created_at")]
+        public DateTime createdAt;
+
+        //[JsonProperty(PropertyName = "side")]
+        //public OrderSide side = null;
+
+        public int GetUnixTime()
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan diff = createdAt - origin;
+            return (int)Math.Floor(diff.TotalSeconds);
+        }
+    }
+}
