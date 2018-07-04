6f0f329ece41caecc139b73945d3ce610796e02c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-04 15:25:05 +0300

    Енамы описывающие состояния ордеров и их типы

---
+++ KunaWrapper/KunaWrapper/DataLayer/Enums/OrderType.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace KunaWrapper.DataLayer.Enums
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum OrderType
+    {
+        limit,
+        market
+    }
+}
