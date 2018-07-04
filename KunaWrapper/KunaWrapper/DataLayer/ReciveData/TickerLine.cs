067b3227f345075e80cadc84bb70c9fb0ae86da3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-04 15:02:18 +0300

    Получаемые объект последних данных с биржи

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/TickerLine.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace CunaWrapper.DataLevel.RciveData
+{
+    public class TickerLine
+    {
+        [JsonProperty(PropertyName = "at")]
+        public int at;
+
+        [JsonProperty(PropertyName = "ticker")]
+        public Ticker ticker;
+    }
+
+    public class Ticker
+    {
+        [JsonProperty(PropertyName = "buy")]
+        public decimal buy;
+
+        [JsonProperty(PropertyName = "sell")]
+        public decimal sell;
+
+        [JsonProperty(PropertyName = "low")]
+        public decimal low;
+
+        [JsonProperty(PropertyName = "high")]
+        public decimal high;
+
+        [JsonProperty(PropertyName = "last")]
+        public decimal last;
+
+        [JsonProperty(PropertyName = "vol")]
+        public decimal vol;
+
+        [JsonProperty(PropertyName = "amount")]
+        public decimal amount;
+    }
+}
