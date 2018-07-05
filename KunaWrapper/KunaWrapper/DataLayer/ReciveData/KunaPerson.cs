8a52d5ab4bd1b903a5fc76a317c3dc1bc8bf069a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-05 19:06:41 +0300

    Класс пользователя, содержащий активы.

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/KunaPerson.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper.DataLayer.ReciveData
+{
+    public class KunaPerson
+    {
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; private set; }
+
+        [JsonProperty(PropertyName = "activated")]
+        public bool IsAktivated { get; private set; }
+
+        [JsonProperty(PropertyName = "accounts")]
+        public List<Assets> Assetses { get; private set; }
+
+    }
+
+    public class Assets
+    {
+        [JsonProperty(PropertyName = "currency")]
+        public string Currency { get; private set; }
+
+        [JsonProperty(PropertyName = "balance")]
+        public decimal Ballans { get; private set; }
+
+        [JsonProperty(PropertyName = "locked")]
+        public decimal LockedSum { get; private set; }
+    }
+}
