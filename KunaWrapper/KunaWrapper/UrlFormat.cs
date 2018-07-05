b9e40031e1e30901542692136ff4eb33ed66ab4b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-05 18:44:29 +0300

    На будущее... класс который будет генерить параметр url необходимый для возврата в главных методах клиента

---
+++ KunaWrapper/KunaWrapper/UrlFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper
+{
+    public static class UrlFormat
+    {
+        public static string Timestamp
+        {
+            get
+            {
+                return "/api/v2/timestamp";
+            }
+        }
+    }
+}
