7600ec8e05cddf06d67fed23cd0f197dd215a518
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-07 07:28:56 +0300

    Строковое представление методов биржи через статические свойства

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/KunaMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper.DataLayer.RequestData
+{
+    public static class KunaMethods
+    {
+        //public methods
+
+        public static string Timestamp { get { return "/api/v2/timestamp"; } }
+
+        public static string Orderbook { get { return "/api/v2/order_book?market={0}"; } }
+
+        public static string Tickerline{ get { return "/api/v2/tickers/{0}"; } }
+
+        public static string Trades { get { return "/api/v2/trades?market={0}"; } }
+
+        public static string Depth { get { return "/api/v2/depth?market={0}"; } }
+
+
+        //private methods
+
+        public static string KunaPerson{ get { return "api/v2/members/me";} }
+
+        //public static string Orders { get { return "/api/v2/orders?market={0}"; } }
+    }
+}
