a84e78a2912de52a14d9f1768a240f97c055318f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-17 09:59:18 +0300

    мелкая оптимизация

--- KunaWrapper/KunaWrapper/DataLayer/RequestData/KunaMethod.cs
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/KunaMethod.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace KunaWrapper.DataLayer.RequestData
 {
-    public static class KunaMethods
+    public static class KunaMethod
     {
         //public methods
 
         public static string Timestamp { get { return "/api/v2/timestamp"; } }
 
-        public static string Orderbook { get { return "/api/v2/order_book?market={0}"; } }
-
         public static string Tickerline{ get { return "/api/v2/tickers/{0}"; } }
+
+        public static string Orderbook { get { return "/api/v2/order_book?market={0}"; } }
 
         public static string Trades { get { return "/api/v2/trades?market={0}"; } }
 
@@ -21,7 +21,7 @@ namespace KunaWrapper.DataLayer.RequestData
 
         //private methods
 
-        public static string KunaPerson{ get { return "api/v2/members/me";} }
+        public static string KunaPerson{ get { return "/api/v2/members/me";} }
 
         //public static string Orders { get { return "/api/v2/orders?market={0}"; } }
     }
