1fc74d737aa6d8272ca03ee92895071d284da69f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 18:58:19 +0200

    Create CurrencyId.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/CurrencyId.cs
@@ -0,0 +1,29 @@
+﻿namespace KunaWrapper.DataLayer
+{
+    public static class CurrencyId
+    {
+        public const string btc_uah  = "btcuah";
+        public const string eth_uah  = "ethuah";
+        public const string xrp_uah  = "xrpuah";
+        public const string ltc_uah  = "ltcuah";
+        public const string zec_uah  = "zecuah";
+        public const string dash_uah = "dashuah";
+        public const string bch_uah  = "bchuah";
+        public const string xlm_uah  = "xlmuah";
+        public const string gbg_uah  = "gbguah";
+        public const string eos_uah  = "eosuah";
+        public const string xem_uah  = "xemuah";
+        public const string krb_uah  = "krbuah";
+        public const string wave_uah = "wavesuah";
+
+        public const string tusd_uah = "tusduah";
+        public const string ausd_uah = "ausduah";
+
+        public const string btc_ausd = "btcausd";
+
+        public const string bch_btc  = "bchbtc";
+        public const string kun_btc  = "kunbtc";
+        public const string eos_btc  = "eosbtc";
+        public const string eth_btc  = "ethbtc";
+    }
+}
