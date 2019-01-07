db3b54a17352720c1ee5f3bc779d0e0070573916
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 20:25:53 +0200

    Rename
    RequestTrades --> RequestTradeHistory

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestTradeHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    internal class RequestTradeHistory : BaseRequest
+    {
+        public RequestTradeHistory(string pairId, ushort limit) : base()
+        {
+            queryUrl = "/api/v2/trades";
+
+            arguments["market"] = pairId;
+            arguments["limit"]  = limit.ToString();
+        }
+    }
+}
