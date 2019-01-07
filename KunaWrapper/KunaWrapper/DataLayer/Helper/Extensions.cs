41ee4ac22c58e741e46b33972ff11cf4bcf43189
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 11:07:41 +0200

    Create Extensions.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/Helper/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using static System.Net.WebUtility;
+
+namespace KunaWrapper.DataLayer.Helper
+{
+    public static class Extensions
+    {
+        public static string ToKeyValueString(this IDictionary<string, string> dict, bool escape = true) =>
+                      string.Join("&", dict.Select(kvp => string.Format("{0}={1}", kvp.Key, escape ? UrlEncode(kvp.Value) : kvp.Value)));
+    }
+}
