e330d857d762ac0815b0645fcd16e9c22abf45e0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 19:23:15 +0200

    Rename
    SignParams --> AuthData

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/AuthData.cs
@@ -0,0 +1,14 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public sealed class AuthData
+    {
+        internal string PublicKey { get; private set; }
+        internal string SecretKey { get; private set; }
+
+        public AuthData(string pubKey, string secKey)
+        {
+            PublicKey = pubKey;
+            SecretKey = secKey;
+        }
+    }
+}
