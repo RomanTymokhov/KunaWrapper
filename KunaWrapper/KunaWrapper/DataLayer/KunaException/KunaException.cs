75872ecca865112ae85422ebe86bab9832a3e237
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 11:06:51 +0200

    Rename
    Rename  KunaApiException  --> KunaException

---
+++ KunaWrapper/KunaWrapper/DataLayer/KunaException/KunaException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace KunaWrapper.DataLayer.KunaException
+{
+    public class KunaException : Exception
+    {
+        public KunaException() { }
+
+        public KunaException(string message) : base(message) { }
+
+        public KunaException(string message, Exception innerException) : base(message, innerException) { }
+
+        public static void CheckException(HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.NotFound)
+                throw new KunaException(response.ReasonPhrase);
+            else
+            {
+                var exception = JsonConvert.DeserializeObject<Error>(response.Content.ReadAsStringAsync().Result);
+
+                throw new KunaException("Ошибка подключения к Kuna:" +
+                                            $"{Environment.NewLine} errorCode: {exception.ErrorMessage.Code} " +
+                                            $"{Environment.NewLine} errorMessage: {exception.ErrorMessage.Message}");
+            }            
+        }
+    }
+}
