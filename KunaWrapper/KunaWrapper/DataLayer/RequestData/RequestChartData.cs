b348a3da8ae22344f125d47bbbfb93c3177710a4
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 19:47:10 +0200

    Update RequestChartData.cs

--- KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestChartData.cs
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestChartData.cs
@@ -1,28 +1,14 @@
-﻿using System.Text;
-using KunaWrapper.DataLayer.Enums;
-using KunaWrapper.DataLayer.RequestData.FrameData;
-using System;
-using System.Collections.Generic;
-
-namespace KunaWrapper.DataLayer.RequestData
+﻿namespace KunaWrapper.DataLayer.RequestData
 {
     internal class RequestChartData : BaseRequest
     {
-        public RequestChartData(MarketPair pair, ChartFrame frame) : base()
+        public RequestChartData(string pairId, string period, ushort limit) : base()
         {
-            Url = "/api/v2/k.json";
+            queryUrl = "/api/v2/k.json";
 
-            RequestArgs["market"] = pair.ToString();
-            RequestArgs["period"] = frame.period.ToString();
-            RequestArgs["limit"] = frame.select.ToString();
-        }
-
-        public override string ToString()
-        {
-            var url = new StringBuilder(Url);
-            url.AppendFormat("?{0}", BuildRequestData(RequestArgs));
-
-            return url.ToString();
+            arguments["market"] = pairId;
+            arguments["period"] = period;
+            arguments["limit"]  = limit.ToString();
         }
     }
 }
