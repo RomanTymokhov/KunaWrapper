12e3b039bea2d219f81395b589a8101331e1b972
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 20:27:46 +0200

    Create PrivateClient.cs

---
+++ KunaWrapper/KunaWrapper/PrivateClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using KunaWrapper.DataLayer.ReciveData;
+using KunaWrapper.DataLayer.RequestData;
+
+namespace KunaWrapper
+{
+    public sealed class PrivateClient : BaseClient
+    {
+        public PrivateClient(string apiKey, string apiSec) : base(apiKey, apiSec) { }
+
+        public async Task<Holder> GetHolderInfoAsync() =>
+                await GetJsonAsync<Holder>(new RequestHolderInfo(authData));
+
+        public async Task<List<Order>> GetHolderOrdersAsync(string pairId) =>
+                await GetJsonAsync<List<Order>>(new RequestHolderOrders(authData, pairId));
+
+        public async Task<List<Trade>> GetHolderTradesAsync(string pairId) =>
+                await GetJsonAsync<List<Trade>>(new RequestHolderTrades(authData, pairId));
+
+        /// <summary>
+        /// Place Order
+        /// </summary>
+        /// <param name="ordeType"> _buy or _sell</param>
+        /// <param name="pairId">Currecy pair identifikator</param>
+        /// <param name="volume">Base Volume</param>
+        /// <param name="price">Base Price</param>
+        /// <returns>Order</returns>
+        public async Task<Order> PlaceOrderAsync(string ordeType, string pairId, decimal volume, decimal price) =>
+                await PostJsonAsync<Order>(new RequestPlaceOrder(authData, ordeType, pairId, volume, price));
+
+        public async Task<Order> CancelOrderAsync(uint orderId) =>
+                await PostJsonAsync<Order>(new RequestCancelOrder(authData, orderId));
+    }
+}
