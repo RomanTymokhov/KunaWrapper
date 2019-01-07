13bf6f2663547433092944462f2637249507c762
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 20:13:18 +0200

    Remove
    enum  --> ServiceTypes

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/ServiceTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public static class ServiceTypes
+    {
+        //------------ Order Type ----------//
+        public const string _buy  = "buy";
+        public const string _sell = "sell";
+
+        //------------ Order Side ----------//
+        public const string _bid = "bid";
+        public const string _ask = "ask";
+
+        //------------ Order Side ----------//
+        public const string _wait   = "wait";
+        public const string _cancel = "cancel";
+
+        //------------ Market Type ----------//
+        public const string _limit  = "limit";
+        public const string _market = "market";
+    }
+}
