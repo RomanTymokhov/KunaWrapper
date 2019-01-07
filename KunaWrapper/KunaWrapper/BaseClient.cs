e7f057013d2841b17f077df77dc493eb5b4657a6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 20:27:19 +0200

    Update BaseClient.cs

--- KunaWrapper/KunaWrapper/BaseClient.cs
+++ KunaWrapper/KunaWrapper/BaseClient.cs
@@ -2,16 +2,17 @@
 using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
-using KunaWrapper.DataLayer.KunaException;
 using KunaWrapper.DataLayer.RequestData;
+using KunaWrapper.DataLayer.KunaException;
+using KunaWrapper.DataLayer.Helper;
 using Newtonsoft.Json;
 
 namespace KunaWrapper
 {
     public abstract class BaseClient : IDisposable
     {
-        private readonly HttpClient httpClient;
-        private readonly AuthData signParams;
+        private  readonly HttpClient httpClient;
+        internal readonly AuthData authData;
 
         public BaseClient()
         {
@@ -19,12 +20,12 @@ namespace KunaWrapper
         }
         public BaseClient(string pubKey, string secKey) : this()
         {
-            signParams = new AuthData(pubKey, secKey);
+            authData = new AuthData(pubKey, secKey);
         }
 
         protected async Task<T> GetJsonAsync<T>(BaseRequest request)
         {
-            var response = await httpClient.GetAsync(request.ToString()).ConfigureAwait(false);
+            var response = await httpClient.GetAsync(request.Url).ConfigureAwait(false);
 
             EnsureSuccessStatusCodeAsync(response);
 
@@ -33,8 +34,9 @@ namespace KunaWrapper
 
         protected async Task<T> PostJsonAsync<T>(BaseRequest request)
         {
-            var response = await httpClient.PostAsync(request.Url,
-                new StringContent(request.ToString(), Encoding.UTF8, "application/x-www-form-urlencoded")).ConfigureAwait(false);
+            var response = await httpClient.PostAsync(request.queryUrl,
+                new StringContent(request.arguments.ToKeyValueString(), 
+                    Encoding.UTF8, "application/x-www-form-urlencoded")).ConfigureAwait(false);
 
             EnsureSuccessStatusCodeAsync(response);
 
