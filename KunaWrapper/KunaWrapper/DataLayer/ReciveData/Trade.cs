aae6960af35dfd35fed35ce1930e2014b3d36dc3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 20:26:56 +0200

    Update Trade.cs

--- KunaWrapper/KunaWrapper/DataLayer/ReciveData/Trade.cs
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/Trade.cs
@@ -1,55 +1,40 @@
 ﻿using System;
 using Newtonsoft.Json;
-using KunaWrapper.DataLayer.Enums;
-using System.Globalization;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
 
 namespace KunaWrapper.DataLayer.ReciveData
 {
     public class Trade
     {
         [JsonProperty("id")]
-        public uint TradeId { get; set; }
-
-        [JsonProperty("price")]
-        private readonly string price;
-        public decimal CoinPrice => price != null ? Convert.ToDecimal(price, CultureInfo.InvariantCulture) : -1;
-
-        [JsonProperty("volume")]
-        private readonly string volume;
-        public decimal BaseVolume => volume != null ? Convert.ToDecimal(volume, CultureInfo.InvariantCulture) : -1;
-
-        [JsonProperty("funds")]
-        private readonly string funds;
-        public decimal RelativeVolume => funds != null ? Convert.ToDecimal(funds, CultureInfo.InvariantCulture) : -1;
+        public uint TradeId { get; private set; }
+        
+        private readonly decimal price;
+        public decimal Price => price;
+        
+        private readonly decimal volume;
+        public decimal BaseVolume => volume;
+        
+        private readonly decimal funds;
+        public decimal QuotedVolume => funds;
 
         [JsonProperty("market")]
-        public MarketPair MarketPair { get; set; }
+        public string CurrencyId { get; private set; }
 
         [JsonProperty("created_at")]
-        public DateTime CreatedTime { get; set; }
+        public DateTime CreatedTime { get; private set; }  //check responce this fild
 
         [JsonProperty("side")]
-        private readonly string side;
-        public OrderSide OrderSide => GetOrderSide(side);
+        public string OrderSide { get; private set; }
 
-        public uint GetUnixTime()
+        [JsonConstructor]
+        public Trade(string price, string volume, string funds)
         {
-            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            TimeSpan diff = CreatedTime - origin;
-            return (uint)Math.Floor(diff.TotalSeconds);
-        }
-
-        private OrderSide GetOrderSide(string side)
-        {
-           switch(side)
-            {
-                case "buy": return OrderSide.buy; 
-                case "sell": return OrderSide.sell;
-                case "bid": return OrderSide.bid;
-                case "ask": return OrderSide.ask;
-                default: return OrderSide.noll;
-
-            }
+            decimal.TryParse(price,  Any, InvariantCulture, out this.price);
+            decimal.TryParse(volume, Any, InvariantCulture, out this.volume);
+            decimal.TryParse(funds,  Any, InvariantCulture, out this.funds);
         }
     }
 }
