1dba91ea3c04fdf6f4b377a6fbfe5035ac0ceaf1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-08 12:04:36 +0200

    Update PublicClient.cs

--- KunaWrapper/KunaWrapper/PublicClient.cs
+++ KunaWrapper/KunaWrapper/PublicClient.cs
@@ -9,16 +9,25 @@ namespace KunaWrapper
     {
         public PublicClient() : base() { }
 
-
         public async Task<long> ReturnTimestampAsync() => 
                 await GetJsonAsync<long>(new RequestTimestamp());
 
         public async Task<TickerLine> ReturnTickerLineAsync(string pairId) =>
                 await GetJsonAsync<TickerLine>(new RequestTickerline(pairId));
 
+        /// <summary>
+        /// Returns detailed information about the depth
+        /// </summary>
+        /// <param name="pairId">Currency Pair Identifikator</param>
+        /// <returns>OrderBook</returns>
         public async Task<OrderBook> ReturnOrderBookAsync(string pairId) =>
                 await GetJsonAsync<OrderBook>(new RequestOrderbook(pairId));
 
+        /// <summary>
+        /// Returns simplified information about the depth: price & volume
+        /// </summary>
+        /// <param name="pairId"></param>
+        /// <returns>Depthreturns>
         public async Task<Depth> ReturnDepthAsync(string pairId) =>
                 await GetJsonAsync<Depth>(new RequestDepth(pairId));
 
@@ -26,12 +35,19 @@ namespace KunaWrapper
         /// Return Trade History
         /// </summary>
         /// <param name="pairId">Currency Pair Identifikator</param>
-        /// <param name="limit">Limit Trades in History</param>
+        /// <param name="limit">Limit Trades in History max = 1000</param>
         /// <returns>List of Trade</returns>
-        public async Task<List<Trade>> ReturnTradeHistoryAsync(string pairId, ushort limit = 1000) =>
+        public async Task<List<Trade>> ReturnTradeHistoryAsync(string pairId, ushort limit = 10) =>
                 await GetJsonAsync<List<Trade>>(new RequestTradeHistory(pairId, limit));
 
-        public async Task<List<List<float>>> ReturnChartDataAsync(string pairId, string period, ushort limit) =>
-                await GetJsonAsync<List<List<float>>>(new RequestChartData(pairId, period, limit));
+        /// <summary>
+        /// Return Candle Charte, setuping paremetrs
+        /// </summary>
+        /// <param name="pairId">Currency Pair Identifikator</param>
+        /// <param name="period">Candle Period: _15_min, _3-_min, _1_hour, _4_hour, _1_day, _1_week</param>
+        /// <param name="limit">Candle Quantity</param>
+        /// <returns>Chart</returns>
+        public async Task<Chart> ReturnChartDataAsync(string pairId, string period, ushort limit) =>
+                await GetJsonAsync<Chart>(new RequestChartData(pairId, period, limit));
     }
 }
