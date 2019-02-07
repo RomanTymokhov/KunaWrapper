e9f4ecb1d332514afd162f7ea21026552403acbf
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-07 11:09:01 +0200

    Create RequestAllDeposits.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestAllDeposits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestAllDeposits : BaseRequest
+    {
+        public RequestAllDeposits(AuthData sign, ushort page, ushort perPage, bool full, string currencyId, string depositAddressesIds, string payInIds) : base(sign)
+        {
+            queryUrl = "/api/v2/deposits";
+
+            arguments["page"] = page.ToString();
+            arguments["per_page"] = perPage.ToString();
+            arguments["full"] = full.ToString();
+
+            if (currencyId != null) arguments["currency"] = currencyId;
+            if (depositAddressesIds != null) arguments["deposit_address_ids"] = depositAddressesIds;
+            if (payInIds != null) arguments["pay_in_ids"] = payInIds;
+
+            GenerateRequest("GET");
+        }
+    }
+}
