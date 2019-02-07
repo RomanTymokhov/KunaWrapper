8b89149799eddaaac3ec45c5a9972328583f4e77
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-07 09:50:49 +0200

    Create RequestCreateDepositAddress.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestCreateDepositAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestCreateDepositAddress : BaseRequest
+    {
+        public RequestCreateDepositAddress(AuthData sign, string currencyId) : base(sign)
+        {
+            queryUrl = "/api/v2/deposit_address";
+
+            arguments["currency"] = currencyId;
+
+            GenerateRequest("GET");
+        }
+    }
+}
