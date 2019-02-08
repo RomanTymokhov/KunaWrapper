dc008a6e20eb103c7aaa5acd07d00c2ec6a192dc
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-08 09:30:07 +0200

    Create RequestWithdrawalInfo.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestWithdrawalInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestWithdrawalInfo : BaseRequest
+    {
+        public RequestWithdrawalInfo(AuthData sign, string withdrawalId) : base(sign)
+        {
+            queryUrl = "/api/v2/withdrawal";
+
+            arguments["id"] = withdrawalId;
+
+            GenerateRequest("GET");
+        }
+    }
+}
