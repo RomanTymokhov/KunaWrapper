287acfbd384092716c4f87ddc2a0bab7a586b7ff
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-08 06:22:35 +0200

    Create DepositAddress.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/DepositAddress.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaWrapper.DataLayer.ReciveData
+{
+    public class DepositAddress
+    {
+        [JsonProperty("id")]
+        public string Id { get; private set; }
+
+        [JsonProperty("payment_details")]
+        public PaymentDetails PaymentDetails { get; private set; }
+    }
+
+    public class  PaymentDetails
+    {
+        [JsonProperty("address")]
+        public string Address { get; private set; }
+    }
+}
