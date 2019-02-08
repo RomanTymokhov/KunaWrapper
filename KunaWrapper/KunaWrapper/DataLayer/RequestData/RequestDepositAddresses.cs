c49262825311ccca2fb282eb25eb6e91b3906e2c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-08 07:13:55 +0200

    Update RequestDepositAddresses.cs

--- KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestDepositAddresses.cs
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestDepositAddresses.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace KunaWrapper.DataLayer.RequestData
+﻿namespace KunaWrapper.DataLayer.RequestData
 {
     public class RequestDepositAddresses : BaseRequest
     {
