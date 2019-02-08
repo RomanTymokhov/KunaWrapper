0578bcb87b852bba4c12397666a13962bccd4e0d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-08 09:30:23 +0200

    Create RequestWithdrawals.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestWithdrawals.cs
@@ -0,0 +1,17 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestWithdrawals : BaseRequest
+    {
+        public RequestWithdrawals(AuthData sign, ushort page, ushort perPage, string currencyId) : base(sign)
+        {
+            queryUrl = "/api/v2/withdrawals";
+
+            arguments["page"] = page.ToString();
+            arguments["per_page"] = perPage.ToString();
+
+            if (currencyId != null) arguments["currency"] = currencyId;
+
+            GenerateRequest("GET");
+        }
+    }
+}
