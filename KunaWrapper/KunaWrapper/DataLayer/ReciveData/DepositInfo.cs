fc28b566be29fd7ebd21947a46e5f6d904fe5c2e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-02-08 08:07:17 +0200

    Create DepositInfo.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/DepositInfo.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+
+namespace KunaWrapper.DataLayer.ReciveData
+{
+    public class DepositInfo
+    {
+        [JsonProperty("id")]
+        public string Id { get; private set; }
+
+        [JsonProperty("currency")]
+        public string CurrencyId { get; private set; }
+
+        private readonly decimal amount;
+        public decimal Amount => amount;
+
+        private readonly decimal fee;
+        public decimal Fee => fee;
+
+        [JsonProperty("created_at")]
+        public string CreatedTime { get; private set; }
+
+        [JsonProperty("state")]
+        public string State { get; private set; }
+
+        [JsonProperty("payment_address_id")]
+        public string OutId { get; private set; }
+
+
+        [JsonConstructor]
+        public DepositInfo(string amount, string fee)
+        {
+            decimal.TryParse(amount, Any, InvariantCulture, out this.amount);
+            decimal.TryParse(fee, Any, InvariantCulture, out this.fee);
+        }
+    }
+}
