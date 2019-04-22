fa78028f0920d3d5b3793f329cf30ee953bc6dd8
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-22 20:27:41 +0300

    Create RequestWithdrawal.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestWithdrawal.cs
@@ -0,0 +1,16 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestWithdrawal : BaseRequest
+    {
+        public RequestWithdrawal(AuthData sign, string currencyId, string address, decimal amount) : base(sign)
+        {
+            queryUrl = "/api/v2/withdrawal";
+
+            arguments["currency"] = currencyId;
+            arguments["address"] = address;
+            arguments["amount"] = amount.ToString(culture);
+
+            GenerateRequest("POST");
+        }
+    }
+}
