e9e24f586fad6e4ffbd47b7d2cf362e3e0055ae2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 07:32:00 +0300

    Create RequestCreateVoucher.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestCreateVoucher.cs
@@ -0,0 +1,21 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestCreateVoucher : BaseRequest
+    {
+        public RequestCreateVoucher(AuthData sign, decimal amount, string currId, string forUser = "all", 
+                                    string nonRefund = null, string comment = null , string privComment = null) : base(sign)
+        {
+            queryUrl = "/api/v2/kuna_codes/issue";
+
+            arguments["amount"] = amount.ToString(culture);
+            arguments["currency"] = currId;
+            arguments["addressee"] = forUser;
+
+            if (nonRefund != null)   arguments["non_refundable_before"] = nonRefund;
+            if (privComment != null) arguments["private_comment"] = privComment;
+            if (comment != null)     arguments["comment"] = comment;
+
+            GenerateRequest("POST");
+        }
+    }
+}
