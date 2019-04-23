d1aab04bf6566087fe993a716d1a2efdc7ebad9f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 10:35:54 +0300

    Create RequestRedeemVouher.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestRedeemVouher.cs
@@ -0,0 +1,14 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestRedeemVouher : BaseRequest
+    {
+        public RequestRedeemVouher(AuthData sign, string codeString) : base(sign)
+        {
+            queryUrl = "/api/v2/kuna_codes/redeem";
+
+            arguments["code"] = codeString;
+
+            GenerateAuthRequest("PUT");
+        }
+    }
+}
