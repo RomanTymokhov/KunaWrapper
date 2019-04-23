60e9cb6fad75f4c6ce596ce0767d8bd769eb4a9b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 07:13:36 +0300

    Create RequestVouchersList.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestVouchersList.cs
@@ -0,0 +1,15 @@
+﻿namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestVouchersList : BaseRequest
+    {
+        public RequestVouchersList(AuthData sign, byte page, short perPage) : base(sign)
+        {
+            queryUrl = "/api/v2/kuna_codes/list";
+
+            arguments["page"] = page.ToString();
+            arguments["per_page"] = perPage.ToString();
+
+            GenerateRequest("GET");
+        }
+    }
+}
