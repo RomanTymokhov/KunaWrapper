6247d0873a55fd07c165140362fb7404bd03a7d7
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 10:45:18 +0300

    Update VouchersClient.cs
    Vouchers --> list, create, check, redeem

--- KunaWrapper/KunaWrapper/VouchersClient.cs
+++ KunaWrapper/KunaWrapper/VouchersClient.cs
@@ -16,7 +16,37 @@ namespace KunaWrapper
         /// <param name="page">The ordinal number of the page in the sample</param>
         /// <param name="perPage">Quantity of elements per page</param>
         /// <returns>List(KuaCode)</returns>
-        public async Task<List<KunaCode>> GetKunaCodesAsync(byte page, short perPage) =>
+        public async Task<List<KunaCode>> GetVouchersAsync(byte page, short perPage) =>
             await GetJsonAsync<List<KunaCode>>(new RequestVouchersList(authData, page, perPage));
+
+        /// <summary>
+        /// Create Kuna Code
+        /// </summary>
+        /// <param name="amount">amount of the creating code</param>
+        /// <param name="currId">currency identifier</param>
+        /// <param name="forUser">recipient of the code (system identifier)</param>
+        /// <param name="nonRefund">non refoundable expirienc date ( in iso8601 )</param>
+        /// <param name="comment">comment for the code</param>
+        /// <param name="privComment">private comment - 255 symbols</param>
+        /// <returns></returns>
+        public async Task<KunaCode> CreateVoucherAsync(decimal amount, string currId, string forUser = "all",
+            string nonRefund = null, string comment = null, string privComment = null) =>
+                await PostJsonAsync<KunaCode>(new RequestCreateVoucher(authData, amount, currId, forUser, nonRefund, comment, privComment));
+
+        /// <summary>
+        /// Check kuna code by first 5 symbols of code
+        /// </summary>
+        /// <param name="codeString_firstFiveSimbols">code string first five simbols</param>
+        /// <returns></returns>
+        public async Task<KunaCode> CheckVoucherAsync(string codeString_firstFiveSimbols) =>
+            await GetJsonAsync<KunaCode>(new RequestCheckVouher(authData, codeString_firstFiveSimbols));
+
+        /// <summary>
+        /// Redeem code
+        /// </summary>
+        /// <param name="codeString"> code string</param>
+        /// <returns></returns>
+        public async Task<KunaCode> RedeemVouherAsync(string codeString) =>
+            await PutJsonAsync<KunaCode>(new RequestRedeemVouher(authData, codeString));
     }
 }
