effcd1261fa38981c4f93f4822a3258ab2358897
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 10:26:20 +0300

    Create RequestCheckVouher.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/RequestData/RequestCheckVouher.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace KunaWrapper.DataLayer.RequestData
+{
+    public class RequestCheckVouher : BaseRequest
+    {
+        public RequestCheckVouher(AuthData sign, string code) : base(sign)
+        {
+            var sb = new StringBuilder("/api/v2/kuna_codes/check/");
+
+            queryUrl = sb.Append(code).ToString();
+        }
+    }
+}
