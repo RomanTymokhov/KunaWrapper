5a691079b54b8879bdd3158ff0bfe197307c4813
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-23 07:13:28 +0300

    Create KunaCode.cs

---
+++ KunaWrapper/KunaWrapper/DataLayer/ReciveData/KunaCode.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaWrapper.DataLayer.ReciveData
+{
+    public class KunaCode
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("code")]
+        public string CodeString { get; set; }
+
+        [JsonProperty("receiver")]
+        public string Receiver { get; set; }
+
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("non_refundable_before")]
+        public string NonRefundableBefore { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("private_comment")]
+        public string PrivateComment { get; set; }
+    }
+}
